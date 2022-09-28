2944c58e13617e8c34697605b7575e193309e2d4
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-09-28 21:55:52 +0530

    Update Main.py

--- Raspberry_Pi/Main.py
+++ Raspberry_Pi/Main.py
@@ -1,2 +1,16 @@
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+cap.set(3, 480)
+cap.set(4, 320)
+
+_, frame = cap.read()
+rows, cols, _ = frame.shape
+
+
+x_medium = int(cols / 2)
+center = int(cols / 2)
+position = 90 # degrees
