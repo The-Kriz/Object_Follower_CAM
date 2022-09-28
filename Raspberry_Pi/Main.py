e3e3ff668eac9b83bfdda38b2fec7e2fa6745904
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-09-28 21:56:18 +0530

    Update Main.py

--- Raspberry_Pi/Main.py
+++ Raspberry_Pi/Main.py
@@ -25,3 +25,13 @@ while True:
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
     contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+
+    
+        for cnt in contours:
+        (x, y, w, h) = cv2.boundingRect(cnt)
+
+        x_medium = int((x + x + w) / 2)
+        break
+
+    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
+
