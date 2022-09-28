c5e6f9d5bc63388ef9e33e4fcb81cc9c342bcf47
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-09-28 21:56:28 +0530

    Update Main.py

--- Raspberry_Pi/Main.py
+++ Raspberry_Pi/Main.py
@@ -26,8 +26,7 @@ while True:
     contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-    
-        for cnt in contours:
+    for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
 
         x_medium = int((x + x + w) / 2)
@@ -35,3 +34,24 @@ while True:
 
     cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
 
+
+
+    # Move servo motor
+    if x_medium < center - 30:
+        if position <= 178:
+            position += 1
+    elif x_medium > center + 30:
+        if position >= 2 :
+            position -= 1
+
+    print(position)
+
+    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
